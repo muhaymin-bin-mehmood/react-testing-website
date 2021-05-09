2e5ba5c8b9a11108d01f149ef5cebc5337f5b3dd
muhaymin-bin-mehmood <muhayminkhan111@gmail.com>
2021-05-10 02:40:48 +0500

    changes-in-website

--- src/components/Featuresec/FeatureSec.jsx
+++ src/components/Featuresec/FeatureSec.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import FeaturedImg from '../../images/feature-sec-img.png'
-
+import './feature.css';
+import FeaturedImg from '../../images/feature-sec-img.png';
+import PerformanceIcon from '../../images/speed-testing-icon.png';
+import prototyping from '../../images/prototyping-icon.png';
+import vector from '../../images/vector-icon.png';
 const FeatureSec = () => {
     return (
         <>
-            <div className="featured-sec">
+            <div className="featured-sec s-pad">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-6">
@@ -13,16 +16,40 @@ const FeatureSec = () => {
                             </div>
                         </div>
                         <div className="col-md-6 align-self-center">
-                            <h1 className="fw-bold mb-1 mb-md-0">Best app for your modern lifestyle</h1>
-                            <p className="my-0 my-md-4">
+                            <h2 className="fw-bold mb-1 mb-md-0">Awesome apps features</h2>
+                            <p className="mb-3 mb-md-5">
                                 Increase productivity with a simple to-do app. app for
                                 managing your personal budgets.
                             </p>
                             <ul>
                                 <li>
-                                    
+                                    <img src={PerformanceIcon} alt="fast performance icon" />
+                                    <div>
+                                        <h3 className="h5">Fast Performance</h3>
+                                        <p>Get your blood tests delivered at home collect a sample from the news your blood tests.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src={prototyping} alt="fast performance icon" />
+                                    <div>
+                                        <h3 className="h5">Prototyping</h3>
+                                        <p>Get your blood tests delivered at home collect a sample from the news your blood tests.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <img src={vector} alt="fast performance icon" />
+                                    <div>
+                                        <h3 className="h5">Vector Editing</h3>
+                                        <p>Get your blood tests delivered at home collect a sample from the news your blood tests.</p>
+                                    </div>
                                 </li>
                             </ul>
+                        </div>
+                        <div className="col-12">
+                            <div className="jackpot">
+                                <h3 className="h1">Smart jackpots that you may love this anytime & anywhere</h3>
+                                <p>The rise of mobile devices transforms the way we consume information entirely and the world's most elevant channels such as Facebook.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
